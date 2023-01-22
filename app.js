23bc8b6c73a41a2fdea616985ea06aea44e00eba
Rebecca Troy <20088744@mail.wit.ie>
2023-01-22 15:46:05 +0000

    prepare for front end admin design
    This commit prepares the code for front end design
    by setting up the css file as required.
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static("public"));
 app.use('/', indexRouter);
 app.use('/hj9h8765qzf5jizwwnua', adminRouter);
 
